affb22a9d478247a37bec81219e6d2879f4a304f
melvintk8 <korsnesmelvin@gmail.com>
2024-10-27 16:45:38 +0100

    java,bilder

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let colors = ["#a8c0ff", "#8ac6d1", "#bbded6", "#fae3d9", "#ffb6b9"];
+  let currentIndex = 0;
 
+  setInterval(() => {
+    const nextIndex = (currentIndex + 1) % colors.length;
+    document.body.style.background = `linear-gradient(to right, ${colors[currentIndex]}, ${colors[nextIndex]})`;
+    document.body.style.transition = "background 10s ease-in-out";
+    currentIndex = nextIndex;
+  }, 10000); // Endre farge hvert 10. sekund
+});
