c3f06a76fac6c7d208a721793fd225e72b08fa86
Hanna Steinhagen <hanna.steinhagen@gmail.com>
2024-11-05 13:30:41 +0100

    lagt til funksjonalitet som gjør at statusbar oppdaterer layout når vinduet resizes

--- JavaScript/statusbar.js
+++ JavaScript/statusbar.js
@@ -7,13 +7,9 @@ export function addStatusBar(app) {
     let remainingItems = 5;
 
     const statusBar = new Container();
-    
     app.stage.addChild(statusBar);
 
-    // Lage ensfargig bakgrunn i form av rektangel
     const background = new Graphics();
-    background.rect(0, 0, app.screen.width, app.screen.height/10);
-    background.fill(0x333333);
 
     // Lage tekst for level/liv/items
     const style = new TextStyle({
@@ -25,24 +21,40 @@ export function addStatusBar(app) {
     const levelText = new Text({
         text: `Level: ${level}`,
         style,
-    })
-    levelText.x = app.screen.width / 10;
-    levelText.y = app.screen.height / 40;
+    });
 
     const livesText = new Text({
         text: `Lives: ${lives}`,
         style,
-    })
-    livesText.x = (app.screen.width / 10) *4;
-    livesText.y = app.screen.height / 40;
-
+    });
+   
     const itemsText = new Text({
         text: `Remaining items: ${remainingItems}`,
         style,
-    })
-    itemsText.x = (app.screen.width / 10) *7;
-    itemsText.y = app.screen.height / 40;
+    });
     
-    statusBar.addChild(background);
-    statusBar.addChild(levelText, livesText, itemsText);
+    //Legge til bakgrunn og tekst
+    statusBar.addChild(background, levelText, livesText, itemsText);
+
+    function updateLayout() {
+        // Lage ensfargig bakgrunn i form av rektangel
+        background.rect(0, 0, app.screen.width, app.screen.height/10);
+        background.fill(0x333333);
+
+        //Oppdate posisjonene til tekstene
+        levelText.x = app.screen.width / 10;
+        levelText.y = app.screen.height / 40;
+        livesText.x = (app.screen.width / 10) *4;
+        livesText.y = app.screen.height / 40;
+        itemsText.x = (app.screen.width / 10) *7;
+        itemsText.y = app.screen.height / 40;
+    };
+    
+    updateLayout();
+
+    window.addEventListener('resize', () => {
+        updateLayout();
+    })
+
+
 }
